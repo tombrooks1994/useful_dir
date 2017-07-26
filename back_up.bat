7b56a3e6129cf63be8eab7cec875729f9edce173
Tom Brooks <t.s.brooks@2013.ljmu.ac.uk>
2017-07-26 13:03:03 +0100

    back_up.bat
    A batch file to back up a folder and copy it to a new folder. It contains the date and time on the directory name.

---
+++ back_up.bat
@@ -0,0 +1,7 @@
+mkdir <back-up directory>
+
+For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set mydate=%%c-%%a-%%b)
+For /f "tokens=1-2 delims=/:" %%a in ('time /t') do (set mytime=%%a%%b)
+XCOPY "<from directory>" "<to directory>%mydate%_%mytime%" /D /E /C /R /I /K /Y 
+
+OK!
